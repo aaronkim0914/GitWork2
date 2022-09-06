9491641a72729526d1c43fc4fcacd2e4db02d3fb
kimaaron <kimaa0914@gmail.com>
2022-09-06 16:21:07 +0900

    <feat> 점프 기능 구현
    <body> 점프 기능 추가

--- Assets/Jumper.cs
+++ Assets/Jumper.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Jumper : MonoBehaviour
 {
     
-   
-   
+   private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         if(Input.GetButtonDown("Jump"))
         {
+            rb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
             Debug.Log("점프한다.");
         }
     }
