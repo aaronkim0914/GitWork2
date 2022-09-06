97323e6537c0a72f17341eaa1d01153373f60c68
kimaaron <kimaa0914@gmail.com>
2022-09-06 16:47:41 +0900

    <feat>상태 패턴 적용

--- Assets/Jumper.cs
+++ Assets/Jumper.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Jumper : MonoBehaviour
 {
-    
-   private Rigidbody rb;
+    public enum JumpState { Ground, Jump, fall }
+    private JumpState state = JumpState.Ground;
 
-    [SerializeField, Range(0f,20f)]
+
+    private Rigidbody rb;
+
+    [SerializeField, Range(0f, 20f)]
     private float jumpPower = 10f;
+
+    private bool isJump = true;
 
     private void Awake()
     {
@@ -18,10 +23,28 @@ public class Jumper : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isJump)
         {
             rb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
             Debug.Log("점프한다.");
         }
+    }
+
+    private IEnumerator Ground()
+    {
+        Debug.Log("땅");
+        yield return null;
+    }
+
+    private IEnumerator Jump()
+    {
+        Debug.Log("점프");
+        yield return null;
+    }
+
+    private IEnumerator Fall()
+    {
+        Debug.Log("추락");
+        yield return null;
     }
 }
